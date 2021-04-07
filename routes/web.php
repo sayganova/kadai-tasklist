541df5d596735125945bec7cba228d4de100b051
sayganova <esayganova@gmail.com>
2021-04-07 06:38:33 +0000

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
